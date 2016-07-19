304cee732abeba2582c11e333d47962d32973acf
Moises Sanchez <moysesray@yahoo.com>
2016-07-18 18:18:35 -0700

    adding file

--- random/js/random.js
+++ random/js/random.js
@@ -1,9 +1,9 @@
-var nUm =[33, 44, 56, 78, 98]
+/*var nUm =[33, 44, 56, 78, 98]
 var sTr =[Gold, Plane, Adrian, heaven, Sulivan]
 var cTy =[Daly city, San Bruno, Oakland, San Mateo, San Jose]
 var sTe =[CA, UT, AZ, ID, NY]
 var zCd =[94402, 04943, 95867,09403,90210]
- 
+ console.log(nUm, sTr, cTy, sTe, zCd);
 
 function getRan(){
      var geTAdrs = '';
@@ -15,34 +15,27 @@ function getRan(){
      }
      return ranDn;
 
+}*/
+
+
+
+var num = Array(523,3452,334,31,5346);
+var street = Array("Gold", "Plane", "Adrian", "Heaven", "Sullivan");
+var city = Array("Daily City", "San Bruno", "Oakland", "San Mateo", "San Jose");
+var state = Array("CA", "UT", "AZ", "ID", "NY");
+var zip = Array(94402, 04943, 95867, 9403,90210);
+
+
+function randomAddress () {
+    var itemNum = num[Math.floor(Math.random()*num.length)];
+    var itemStreet = street[Math.floor(Math.random()*street.length)];
+    var itemState = state[Math.floor(Math.random()*state.length)];
+    var itemZip = zip[Math.floor(Math.random()*zip.length)];
+
+    var a = (itemNum + " " + itemStreet + " " + itemState +  " " + itemZip);
+    return a;
+
 }
+resultado = randomAddress();
+console.log(resultado);
 
-
-/*
-
-
-function randomString() {
- var chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz";
- var string_length = 8;
- var randomstring = '';
- for (var i=0; i<string_length; i++) {
-  var rnum = Math.floor(Math.random() * chars.length);
-  randomstring += chars.substring(rnum,rnum+1);
- }
- document.randform.randomfield.value = randomstring;
-}
-
-
-
-function genMAC(){
-    var hexDigits = "0123456789ABCDEF";
-    var macAddress = "";
-    for (var i = 0; i < 6; i++) {
-        macAddress+=hexDigits.charAt(Math.round(Math.random() * 15));
-        macAddress+=hexDigits.charAt(Math.round(Math.random() * 15));
-        if (i != 5) macAddress += ":";
-    }
-
-    return macAddress;
-}
-*/
