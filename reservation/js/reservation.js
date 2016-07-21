ae2be8edf8605994abb04a54f4491994d6c691b9
Moises Sanchez <moysesray@yahoo.com>
2016-07-20 20:57:25 -0700

    file

---
+++ reservation/js/reservation.js
@@ -0,0 +1,17 @@
+function () {
+	
+}
+
+var reservation = {
+    roomNumber: 101,
+    roomType: "luxury",
+    startDate: "07-01-16",
+    endDate: "07-04-16",
+    smoking: false,
+    roomRate: 275,
+    var beds = {
+        bedsType: "Queen",
+        bedsNumber: 2
+    },
+    occupants: 2
+};
