e5c01e8fa919671e50dc6617b3945583181ddcdf
Moises Sanchez <moysesray@yahoo.com>
2016-07-22 14:39:07 -0700

    Add files via upload

--- calculators/js/formulas.js
+++ calculators/js/formulas.js
@@ -6,3 +6,27 @@
 // 3.	 Be sure to link up this file in your HTML doc
 /////////////////////////////////////////////////////////////////////
 
+//Calculate Celcius to fahrenheit: celcius * (9/5) + 32
+function calcCelciusToFarenheit(celcius) {
+	var fahrenheit = celcius * (9/5) + 32;
+	return fahrenheit;
+}	
+
+//Calculate Fahrenheit to celcius: (fahrenheit - 32) * 5/9
+function calcFahrenheitToCelcius(fahrenheit) {
+	var celcius = (fahrenheit - 32) * 5/9;
+	return celcius;
+}
+
+//Calculate Radius to circumference: 2 * Math.PI * radius
+function calcCircumference(radius) {
+	var circumference = 2 * Math.PI * radius;
+	return circumference;
+}
+
+//Calculate Pythagorean theorem: Math.sqrt((a * a) + (b * b))
+function calcLongestSide(sideA, sideB) {
+	var longestSide = Math.sqrt((sideA * sideA) + (sideB * sideB));
+	return longestSide;
+}
+
